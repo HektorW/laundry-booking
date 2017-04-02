8b4d6b6428c8b1e232b04d90cd8346c77c35dc0f
Hektor Wallin <hektorw@gmail.com>
2017-04-02 20:46:50 +0200

    Initial commit
    Added base server

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "env": {
+    "es6": true,
+    "node": true,
+  },
+  "plugins": ["node"],
+  "extends": ["eslint:recommended", "plugin:node/recommended"],
+    "rules": {
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "single"],
+    "semi": ["error", "never"],
+    "generator-star-spacing": ["error", "after"],
+    "comma-dangle": ["error", "always-multiline"],
+    "require-yield": 0,
+  }
+};
---
+++ index.js
@@ -0,0 +1,11 @@
+const server = require('./server')
+const config = require('./config')
+const log = require('./log')(__filename)
+
+
+log.info(config, 'starting laundry-booking app')
+
+
+server.listen(config.port, () => {
+  log.info({ port: config.port }, 'laundry-booking server started')
+})
